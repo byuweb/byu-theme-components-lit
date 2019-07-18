fea4673a0960f90196b1cd57e347c57150ac2f00
Blake Morgan <blake_morgan@byu.edu>
2019-07-18 17:17:46 -0600

    Remove script loader function to seperate package.

--- components/byu-theme-components.js
+++ components/byu-theme-components.js
@@ -1,25 +1,7 @@
 'use strict'
 
-import '@byuweb/web-components-loader'
+import load from '@byuweb/web-components-loader'
 import { version } from '../package.json'
 
-const myUrl = document.currentScript.src;
-
-window.WebComponents = window.WebComponents || {
-  waitFor (cb) { addEventListener('WebComponentsReady', cb) }
-}
-
-WebComponents.waitFor(() => {
-
-  console.log(`--------------- Starting byu-theme-components ${version} ---------------`);
-  loadScript('components.min.js')
-
-})
-
-function loadScript(relativeUrl) {
-  const url = new URL(relativeUrl, myUrl);
-  const el = document.createElement("script");
-  el.src = url.href;
-  el.type = 'module';
-  document.head.append(el);
-}
+console.log(`--------------- Starting byu-theme-components ${version} ---------------`);
+load('components.min.js', 'https://cdn.byu.edu/web-component-polyfills-v2/experimental/master')
